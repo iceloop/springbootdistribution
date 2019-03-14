ca851f87e19f3a2a5ecbbe52504eab8da0115e92
iceloop <870285972@qq.com>
2019-03-14 13:44:33 +0800

    rt
    rt

---
+++ springboottest/springboot-api/src/main/java/com/gl/springbootapi/controller/Testcontroller.java
@@ -0,0 +1,20 @@
+package com.gl.springbootapi.controller;
+
+import com.gl.springbootutils.helper.Common;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+@RestController
+@RequestMapping("/test")
+public class Testcontroller {
+
+    @GetMapping("/date")
+    public  String getdata(){
+        Date d= Common.getCurDate();
+        SimpleDateFormat  df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+   return  "当前时间"+df.format(d).toString();
+    }
+}
---
+++ springboottest/springboot-utils/src/main/java/com/gl/springbootutils/helper/Common.java
@@ -0,0 +1,17 @@
+package com.gl.springbootutils.helper;
+
+
+import java.util.Date;
+
+public class Common {
+    /**
+     * 获取当前时间
+     * @return
+     */
+
+    public static Date getCurDate()
+    {
+        Date d = new Date();
+        return d;
+    }
+}
