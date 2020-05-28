6a7c1bef3d094bdc0782a758e0ab974135a50db3
niuhaoran <hrniu@isoftstone.com>
2020-05-28 14:27:16 +0800

    测试

---
+++ springboottest/springboot-api/src/test/java/com/test.java
@@ -0,0 +1,14 @@
+package com;
+
+public class test {
+
+
+    public static void main(String[] args) {
+          String aa="heelo word   ";
+          String aa1=aa.split(" ")[1].toString();
+        System.out.println(aa1);
+        }
+
+
+    }
+
